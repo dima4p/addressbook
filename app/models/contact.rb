3ae0817440790466b5f565874b0c13a14b7d9fbe
Dmitri Koulikoff <dima@koulikoff.ru>
2012-10-07 19:09:25 +0400

    Method Contact.merge_csv is created

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,14 +4,16 @@ class Contact < ActiveRecord::Base
 
   belongs_to :user
 
-  validates :first_name, presence: true
+  validates :first_name, :user_id, presence: true
   acts_as_paranoid
 
   COLUMNS = %w[id first_name last_name middle_name phone updated_at deleted_at]
+  CSV_OPTIONS = {col_sep: "\t"}
 
   class << self
+
     def to_csv(rel)
-      CSV.generate(col_sep: "\t") do |csv|
+      CSV.generate(CSV_OPTIONS) do |csv|
         csv << COLUMNS
         rel.with_deleted.find_each do |contact|
           csv << COLUMNS.inject([]) do |acc, attr|
@@ -21,7 +23,58 @@ class Contact < ActiveRecord::Base
           end
         end
       end
-    end
-  end
+    end     # to_csv
+
+    def merge_csv(string, user)
+      total = created = updated = undeleted = deleted = skipped = errors = 0
+      messages = []
+      logger.debug "Contact@#{__LINE__}.merge_csv #{string.inspect}" if logger.debug?
+      parsed = CSV.parse(string, CSV_OPTIONS.merge(headers: true))
+      total = parsed.size
+      parsed.each do |csv_record|
+        logger.debug "Contact@#{__LINE__}.merge_csv #{csv_record.to_hash.inspect}" if logger.debug?
+        if (id = csv_record['id']).nil?
+          record = create csv_record.to_hash.merge(user_id: user), without_protection: true
+          logger.debug "Contact@#{__LINE__}.merge_csv #{record.inspect}" if logger.debug?
+          if record.valid?
+            created += 1
+          else
+            errors += 1
+            messages << [csv_record.to_csv(col_sep: "\t"), record.errors.messages]
+          end
+        else
+          record = with_deleted.find_by_id id
+          if record.nil?
+            skipped += 1
+          else
+            logger.debug "Contact@#{__LINE__}.merge_csv #{csv_record['updated_at'].to_time.inspect} #{record.updated_at.inspect}" if logger.debug?
+            if csv_record['deleted_at'].present? and dlt = csv_record['deleted_at'].to_time and
+                record.deleted_at.blank?
+              if record.updated_at.to_s(:db).to_time > dlt
+                skipped += 1
+              else
+                record.delete
+                deleted += 1
+              end
+            elsif (upd = csv_record['updated_at'].to_time) >= record.updated_at.to_s(:db).to_time and
+                (record.deleted_at.nil? or upd >= (dlt = record.deleted_at.to_s(:db).to_time))
+              record.update_attributes csv_record.to_hash.merge(deleted_at: nil), without_protection: true
+              if record.valid?
+                updated += 1
+                undeleted += 1 if dlt
+              else
+                errors += 1
+                messages << [csv_record.to_csv(col_sep: "\t"), record.errors.messages]
+              end
+            else
+              skipped += 1
+            end
+          end
+        end
+      end
+      {total: total, created: created, updated: updated, deleted: deleted, undeleted: undeleted,  skipped: skipped, errors: errors, messages: messages}
+    end     # merge_csv
+
+  end   # class << self
 
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 
+HEADER = "id\tfirst_name\tlast_name\tmiddle_name\tphone\tupdated_at\tdeleted_at"
+
 describe Contact do
 
-  subject { create :contact }
+  subject { create :contact, user_id: current_user.id }
 
   it { should be_valid }
+  it { should validate_presence_of :user_id }
   it { should validate_presence_of :first_name }
 
   describe :class do
@@ -17,9 +20,93 @@ describe Contact do
       res = Contact.to_csv(Contact.order(:id))
       res.should be_a_kind_of String
       res = res.split(/\n/)
-      res.first.should == "id\tfirst_name\tlast_name\tmiddle_name\tphone\tupdated_at\tdeleted_at"
+      res.first.should == HEADER
       res = res.last.split(/\t/)
       res.last.should == c2.deleted_at.to_s(:db)
+    end
+
+    describe 'responding to :merge_csv' do
+      before :each do
+        user = create :active_user
+        @c1 = create :contact, user: user
+        @c2 = create :contact, user: user
+        Contact.count.should == 2
+        current_user.stub(:to_i) {current_user.id}
+      end
+
+      it 'adds new record if no id is set in the file' do
+        res = Contact.merge_csv HEADER + "\n\tnew_name\n", current_user
+        res[:total].should == 1
+        res[:created].should == 1
+        Contact.count.should == 3
+        Contact.last.first_name.should == "new_name"
+      end
+
+      it 'reports errors if new record is not valid' do
+        res = Contact.merge_csv HEADER + "\n\t\tnew_name\n", current_user
+        Contact.count.should == 2
+        res[:created].should == 0
+        res[:errors].should == 1
+        res[:messages].should == [["\t\tnew_name\t\t\t\t\n", {:first_name=>["can't be blank"]}]]
+      end
+
+      it 'ignores the record if it is not found in the DB' do
+        res = Contact.merge_csv HEADER + "\n#{Contact.count + 1}\tnew_name\n", current_user
+        Contact.count.should == 2
+        res[:skipped].should == 1
+      end
+
+      it 'ignores the record if timestamp in the DB is newer' do
+        res = Contact.merge_csv HEADER + "\n#{@c1.id}\tnew_name\t\t\t\t#{@c1.updated_at - 1.hour}\n", current_user
+        Contact.count.should == 2
+        res[:skipped].should == 1
+      end
+
+      it 'ignores the record if the record in the DB is deleted eariler' do
+        res = Contact.merge_csv HEADER + "\n#{@c1.id}\tnew_name\t\t\t\t#{@c1.updated_at - 1.hour}\n", current_user
+        Contact.count.should == 2
+        res[:skipped].should == 1
+      end
+
+      it 'updates the record if timestamp in the DB is not newer' do
+        res = Contact.merge_csv HEADER + "\n#{@c1.id}\tnew_name\t\t\t\t#{@c1.updated_at}\n", current_user
+        Contact.count.should == 2
+        res[:updated].should == 1
+        @c1.reload.first_name.should == 'new_name'
+      end
+
+      it 'reports errors if timestamp in the DB is not newer but the record has errors' do
+        res = Contact.merge_csv HEADER + "\n#{@c1.id}\t\tnew_name\t\t\t#{@c1.updated_at}\n", current_user
+        Contact.count.should == 2
+        res[:errors].should == 1
+        res[:messages].first.last.should == {:first_name=>["can't be blank"]}
+      end
+
+      it 'undeletes and updates the record if the record in the DB is deleted eariler' do
+        @c1.delete
+        Contact.count.should == 1
+        res = Contact.merge_csv HEADER +
+            "\n#{@c1.id}\tnew_name\t\t\t\t#{@c1.updated_at}\t#{@c1.updated_at}\n", current_user
+        Contact.count.should == 2
+        res[:updated].should == 1
+        res[:undeleted].should == 1
+        Contact.find_by_id(@c1).first_name.should == 'new_name'
+      end
+
+      it 'ignores deletion if the record in the DB is updated later' do
+        res = Contact.merge_csv HEADER +
+            "\n#{@c1.id}\tnew_name\t\t\t\t#{@c1.updated_at}\t#{@c1.updated_at - 1.second}\n", current_user
+        Contact.count.should == 2
+        res[:skipped].should == 1
+      end
+
+      it 'deletes the record unless the record in the DB is updated later' do
+        res = Contact.merge_csv HEADER +
+            "\n#{@c1.id}\tnew_name\t\t\t\t#{@c1.updated_at}\t#{@c1.updated_at}\n", current_user
+        Contact.count.should == 1
+        res[:deleted].should == 1
+      end
+
     end
   end
 
