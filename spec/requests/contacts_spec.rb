cff02147121a4c112dd285062dad6718a0f0e026
Dmitri Koulikoff <dima@koulikoff.ru>
2012-10-22 00:33:19 +0400

    Integration Rspecs added

--- spec/requests/contacts_spec.rb
+++ spec/requests/contacts_spec.rb
@@ -1,11 +1,52 @@
 require 'spec_helper'
 
 describe "Contacts" do
+
+  before :each do
+    @user = create :active_user
+    visit "/en/user_sessions/new"
+    within('form#new_user_session') do
+      fill_in 'user_session_email', with: @user.email
+      fill_in 'user_session_password', with: 'password'
+      click_button('Login')
+    end
+    @c1 = create :contact, user: @user
+    @c2 = create :contact, user: @user
+  end
+
   describe "GET /contacts" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get contacts_path
-      # response.status.should be(200)
+    it "shows the list of entries" do
+      visit '/contacts'
+      current_path.should == "/contacts"
+      page.should have_content(@c1.first_name)
+      page.should have_content(@c1.middle_name)
+      page.should have_content(@c1.last_name)
+      page.should have_content(@c2.first_name)
+      page.should have_content(@c2.middle_name)
+      page.should have_content(@c2.last_name)
+    end
+
+    it 'updates the entry', js: true do
+      visit '/contacts'
+      find("a[href='/en/contacts/#{@c1.id}/edit']").click
+      fill_in 'contact_first_name', with: 'New_first_name'
+      click_button 'Update Contact'
+      page.should have_content 'New_first_name'
+    end
+
+    it 'deletes the entry', js: true do
+      visit '/contacts'
+      find("a[href='/en/contacts/#{@c1.id}'].destroy").click
+      page.should_not have_content @c1.first_name
+    end
+
+    it 'creates new entry', js: true do
+      visit '/contacts'
+      find("a[href='/en/contacts/new']").click
+      fill_in 'contact_first_name', with: 'New_first_name'
+      click_button 'Create Contact'
+      page.should have_content 'New_first_name'
+      Contact.count.should == 3
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,7 +24,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
@@ -38,6 +38,18 @@ RSpec.configure do |config|
   config.order = "random"
 
   config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 
   Capybara.default_wait_time = 5
   Capybara.javascript_driver = :webkit
